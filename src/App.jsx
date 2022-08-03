bb6d1907036071a2658bcd6b780c2fd69564234f
Vitaly Davydenko <51551622+vitaly0294@users.noreply.github.com>
2022-08-03 09:51:04 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.scss';
+
+const App = () => {
+  return (
+    <div>
+      Старт
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/index.jsx"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
+    publicPath: "/",
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: "./src/index.html"}),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|s[ac]ss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.m?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+}
